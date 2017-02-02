3e25d1926d08f50eec2db453f1956a5673cb69be
Torrencem <matt@torrencefamily.net>
2017-02-02 09:41:42 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Matt Torrence',
   author_email = 'matt@torrencefamily.net',
   url = 'https://github.com/Torrencem/PyAPL',
-    download_url='https://github.com/Torrencem/PyAPL/tarball/0.1.6',
+    download_url='https://github.com/Torrencem/PyAPL/tarball/0.2.0',
   keywords = ['interpreter', 'APL'],
   classifiers = [],
   install_requires=[
