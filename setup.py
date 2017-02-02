e4ba01021b5ae10130b64d1695739b5d53d47edc
Matt <matt@torrencefamily.net>
2017-02-02 15:59:35 -0500

    Fixed Download URL

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 setup(
   name = 'PyAPL',
   packages = ['PyAPL'],
-    version='0.2.0',
+    version='0.2.1',
   description = 'A Python interpreter for the APL programming language',
   author = 'Matt Torrence',
   author_email = 'matt@torrencefamily.net',
   url = 'https://github.com/Torrencem/PyAPL',
-    download_url='https://github.com/Torrencem/PyAPL/tarball/0.2.0',
+    download_url='https://github.com/Torrencem/PyAPL/tarball/0.2.1',
   keywords = ['interpreter', 'APL'],
   classifiers = [],
   install_requires=[
